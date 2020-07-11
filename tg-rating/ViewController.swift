d94cd7c829a0b3fa414f035ac4391f3903aba7fb
Angel <personal@angelpatricio.mx>
2020-07-11 10:08:33 -0500

    first commit

---
+++ tg-rating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tg-rating
+//
+//  Created by Angel Patricio Rodriguez (Vendor) on 11/07/20.
+//  Copyright © 2020 Angel Patricio Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
